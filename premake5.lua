c20c9b7a6d434ff3015a876a3571a884d52de619
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2022-06-13 20:48:36 +1200

    premake

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
+    targetdir (bin_dir)
+    objdir (int_dir)
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        "imgui_tables.cpp",
+
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
